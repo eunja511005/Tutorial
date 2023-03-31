20e011099e53314a2f1c29f80e922eec7afecf14
eunja511005 <118089135+eunja511005@users.noreply.github.com>
2023-03-31 17:40:32 +0900

    Update schema.sql

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,2 +1,22 @@
-drop table  if exists book;
-create table book (id INT AUTO_INCREMENT, isbn varchar(255), name varchar(255), author varchar(255), primary key (id));
+DROP TABLE emp;
+CREATE TABLE emp
+(
+    empno       NUMBER(4)	NOT NULL,
+    ename       VARCHAR2(10),
+    job         VARCHAR2(9),
+    mgr         NUMBER(4),
+    hiredate    DATE,
+    sal         NUMBER(7,2),
+    comm        NUMBER(7,2),
+    deptno      NUMBER(2),
+    CONSTRAINT emp_pk PRIMARY KEY (empno)
+);
+COMMENT ON TABLE emp IS '사원정보';
+COMMENT ON COLUMN emp.empno IS '사원번호';
+COMMENT ON COLUMN emp.ename IS '성명';
+COMMENT ON COLUMN emp.job IS '직군';
+COMMENT ON COLUMN emp.mgr IS '직속상사';
+COMMENT ON COLUMN emp.hiredate IS '입사일';
+COMMENT ON COLUMN emp.sal IS '급여';
+COMMENT ON COLUMN emp.comm IS '보너스';
+COMMENT ON COLUMN emp.deptno IS '부서코드';
