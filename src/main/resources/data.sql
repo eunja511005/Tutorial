5eba926db9ff8b194801bba33fdf719f8227ce68
ywbest.park <ywbest.park@gmail.com>
2023-03-31 21:34:56 +0900

    :new: add auto_seq.nextval

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,3 @@
 INSERT INTO emp VALUES(1, 'KING', 'PRESIDENT', NULL, TO_DATE('1981-11-17', 'yyyy-mm-dd'), 5000, NULL, 10);
-insert into book(isbn, name, author) values ('isbn1', 'name1', 'author1');
-insert into book(isbn, name, author) values ('isbn2', 'name2', 'author2');
+insert into book(seq, isbn, name, author) values (auto_seq.NEXTVAL, 'isbn1', 'name1', 'author1');
+insert into book(seq, isbn, name, author) values (auto_seq.NEXTVAL, 'isbn2', 'name2', 'author2');
