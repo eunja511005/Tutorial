b813a76ea4ebba4425981d6b235b4ec3658934e0
eunja511005 <eunja511005@gmail.com>
2023-04-01 14:14:21 +0900

    샘플 UI 추가

--- src/main/java/com/eun/tutorial/controller/MyRestController.java
+++ src/main/java/com/eun/tutorial/controller/MyRestController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class MainController {
+public class MyRestController {
 	
 	@RequestMapping
 	public String helloWorld() {
---
+++ src/main/java/com/eun/tutorial/controller/MyWebInitController.java
@@ -0,0 +1,38 @@
+package com.eun.tutorial.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.servlet.view.RedirectView;
+
+import com.eun.tutorial.dto.Book;
+import com.eun.tutorial.service.BookService;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RequiredArgsConstructor
+@Controller
+public class MyWebInitController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(MyWebInitController.class);
+	private final BookService bookService;
+	
+	@GetMapping("/")
+    public ModelAndView viewInit() {
+		logger.debug("request url : /");
+		
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("redirect:/index.html");
+
+        return modelAndView;
+    }
+}
