913a057e4466c323c4af1b9687b8f7fdc081f7b1
eunja511005 <eunja511005@gmail.com>
2023-04-11 10:17:14 +0900

    Spring을 Apache Tomcat 같은 Servlet Container 에서 동작하도록 하기 위해 SpringBootServletInitializer를 상속 추가  Changes not staged for commit: 	modified:   src/main/resources/application.properties

--- src/main/java/com/eun/tutorial/TutorialApplication.java
+++ src/main/java/com/eun/tutorial/TutorialApplication.java
@@ -2,9 +2,16 @@ package com.eun.tutorial;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TutorialApplication {
+public class TutorialApplication extends SpringBootServletInitializer{
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(TutorialApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(TutorialApplication.class, args);
