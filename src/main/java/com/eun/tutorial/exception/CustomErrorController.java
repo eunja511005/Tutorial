2369ca5056f856162587e89923da81c7114805fa
eunja511005 <eunja511005@gmail.com>
2023-04-22 18:12:18 +0900

    :pencil2: 모든 오류 내용 저자 될 수 있도록 수정 #5  Changes to be committed: 	new file:   src/main/java/com/eun/tutorial/exception/CustomErrorController.java 	new file:   src/main/webapp/WEB-INF/jsp/error/401.jsp 	new file:   src/main/webapp/WEB-INF/jsp/error/404.jsp 	new file:   src/main/webapp/WEB-INF/jsp/error/500.jsp 	new file:   src/main/webapp/WEB-INF/jsp/error/503.jsp

---
+++ src/main/java/com/eun/tutorial/exception/CustomErrorController.java
@@ -0,0 +1,90 @@
+package com.eun.tutorial.exception;
+
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.web.ServerProperties;
+import org.springframework.boot.autoconfigure.web.servlet.error.BasicErrorController;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorViewResolver;
+import org.springframework.boot.web.error.ErrorAttributeOptions;
+import org.springframework.boot.web.servlet.error.ErrorAttributes;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.eun.tutorial.dto.ZthhErrorDTO;
+import com.eun.tutorial.service.ZthhErrorService;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Controller
+@RequestMapping("${server.error.path:${error.path:/error}}")
+public class CustomErrorController extends BasicErrorController {
+	
+	@Autowired
+	private ZthhErrorService zthhErrorService;
+
+    public CustomErrorController(ErrorAttributes errorAttributes,
+                                 ServerProperties serverProperties,
+                                 List<ErrorViewResolver> errorViewResolvers) {
+        super(errorAttributes, serverProperties.getError(), errorViewResolvers);
+    }
+
+    @RequestMapping(produces = MediaType.TEXT_HTML_VALUE)
+    public ModelAndView errorHtml(HttpServletRequest request,
+                                  HttpServletResponse response) {
+        
+		HttpStatus status = getStatus(request);
+		ModelAndView modelAndView = new ModelAndView();
+		
+		Map<String, Object> body = getErrorAttributes(request, ErrorAttributeOptions.defaults());
+		saveZthhError(body);
+		
+		switch (status){
+			case UNAUTHORIZED:
+				modelAndView.setViewName("jsp/error/401");
+				break;
+			case NOT_FOUND:
+				modelAndView.setViewName("jsp/error/404");
+				break;
+			case INTERNAL_SERVER_ERROR:
+				modelAndView.setViewName("jsp/error/500");
+				break;
+			case SERVICE_UNAVAILABLE:
+				modelAndView.setViewName("jsp/error/503");
+				break;
+		}
+        
+        return modelAndView;
+    }
+
+    @Override
+    public ResponseEntity<Map<String, Object>> error(HttpServletRequest request) {
+        
+		HttpStatus status = getStatus(request);
+		if (status == HttpStatus.NO_CONTENT) {
+			return new ResponseEntity<>(status);
+		}
+		
+		Map<String, Object> body = getErrorAttributes(request, ErrorAttributeOptions.defaults());
+		saveZthhError(body);
+		
+		return new ResponseEntity<>(body, status);
+        
+    }
+    
+    private void saveZthhError(Map<String, Object> errorMap) {
+    	zthhErrorService.save(ZthhErrorDTO.builder()
+                .errorMessage("MyWebSecurityConfigurerAdapter Error : " + errorMap.toString())
+                .build()
+        );
+    }
+}
---
+++ src/main/webapp/WEB-INF/jsp/error/503.jsp
@@ -0,0 +1,13 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <title>503 Service Unavailable</title>
+</head>
+<body>
+  <h1>503 Service Unavailable</h1>
+  <p>Sorry, the service is currently unavailable. Please try again later.</p>
+</body>
+</html>
