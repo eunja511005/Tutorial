e4215e754ddcb49ea25ade5211eaa13d974f1eb4
eunja511005 <eunja511005@gmail.com>
2023-05-05 21:24:24 +0900

    :new: init Commit  Changes to be committed: 	new file:   replay_pid39776.log 	modified:   src/main/java/com/eun/tutorial/batch/BatchScheduler.java 	modified:   src/main/java/com/eun/tutorial/controller/init/InitController.java 	modified:   src/main/resources/application.properties

--- src/main/java/com/eun/tutorial/batch/BatchScheduler.java
+++ src/main/java/com/eun/tutorial/batch/BatchScheduler.java
@@ -25,7 +25,7 @@ public class BatchScheduler {
      * 초, 분, 시, 일, 월, 주, (년)
      * 초(0-59) 분(0-59) 시간(0-23) 일(1-31) 월(1-12) 요일(0-7)
      */
-    @Scheduled(cron = "0/60 * * * * ?")
+    //@Scheduled(cron = "0/60 * * * * ?")
     public void deleteBatchForZthhError() {
     	printStart("[Batch] Delete ZTHH_ERROR");
     }
--- src/main/java/com/eun/tutorial/controller/init/InitController.java
+++ src/main/java/com/eun/tutorial/controller/init/InitController.java
@@ -1,43 +1,21 @@
 package com.eun.tutorial.controller.init;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.Map;
 
-import org.apache.tika.Tika;
-import org.apache.tika.mime.MediaType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContext;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.web.bind.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
-import org.springframework.util.MimeTypeUtils;
-import org.springframework.util.ObjectUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.multipart.MultipartHttpServletRequest;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.eun.tutorial.dto.UserInfoDTO;
-import com.eun.tutorial.dto.ZthhFileAttachDTO;
-import com.eun.tutorial.service.UserService;
-import com.eun.tutorial.service.ZthhFileAttachService;
 import com.eun.tutorial.service.user.UserDetailsImpl;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-
-import java.io.File;
-import java.io.IOException;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -49,6 +27,34 @@ public class InitController {
 	@GetMapping("/initInit")
 	public ModelAndView init() {
 		logger.debug("request url : /init");
+		
+		
+		
+
+		
+		
+        StringBuilder stringBuilder = new StringBuilder();
+
+        String navbarHeader = "<ul class=\"navbar-nav ms-auto\">";
+        String navbarFooter = "</ul>";
+
+//        stringBuilder.append(navbarHeader);
+//        ZthmMenu current;
+//        for (int i=0; i<zthmMenuMap.get(null).size(); i++){
+//            current = zthmMenuMap.get(null).get(i);
+//            retrieve(current, zthmMenuMap, stringBuilder);
+//        }
+//        stringBuilder.append(navbarFooter);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 		
 		ModelAndView modelAndView = new ModelAndView();
 		modelAndView.setViewName("jsp/init/init");
